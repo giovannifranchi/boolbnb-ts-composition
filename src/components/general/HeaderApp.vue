495510a9534fafc42954f3eec406de6b2c67c6d2
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-07-18 13:03:33 +0200

    Add: logo component with size type

---
+++ src/components/general/HeaderApp.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <div class="container">
+            <LogoComponent size="medium" logoImg="fefwf"/>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+import LogoComponent from '../utils/LogoComponent.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/utils/LogoComponent.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="logo">
+        <img :src="logoImg" alt="logo" :style="computedStyle">
+        <span>Logo</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { computed } from 'vue'
+import type { Size } from '@/types/utils-types/Size'
+
+
+const {logoImg, size} = defineProps({
+    logoImg: {
+        type: String as ()=> string | null,
+        required: true
+    },
+
+    size: {
+        required: true,
+        type: String as ()=> Size
+    }
+})
+
+let computedStyle = computed(() => {
+  let dimension: string
+  switch (size.value) {
+    case 'big':
+      dimension = '100px'
+      break
+    case 'medium':
+      dimension = '70px'
+      break
+    case 'small':
+      dimension = '50px'
+      break
+    default:
+      dimension = ''
+  }
+  return `width: ${dimension}; height: ${dimension};`
+})
+
+</script>
+
+<style scoped>
+
+</style>
