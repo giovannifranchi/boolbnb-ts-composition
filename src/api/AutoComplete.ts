8fb2ffb9430f7a3bb0418977e12f651e84cd0748
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-07-18 18:35:00 +0200

    Add: autocomplete api call

---
+++ src/api/AutoComplete.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const apiKey = import.meta.env.VITE_TOMTOM_API_KEY;
+
+const AutoComplete =  {
+    search: async(query:string)=> {
+        const response = await axios.get(`https://api.tomtom.com/search/2/search/${query}.json?key=${apiKey}&typeahead=true&limit=5`)
+        return response.data; 
+    }
+}
+
+export default AutoComplete
--- src/components/HomeView/JumbotronComponent.vue
+++ src/components/HomeView/JumbotronComponent.vue
@@ -1,25 +1,10 @@
 <template>
-    <div v-if="!store.allApartments">
-        ...loading
+    <div>
+
     </div>
-    <ul v-else>
-        <li v-for="(apartment, index) in store.allApartments" :key="index">
-        {{ apartment.name }}
-        </li>
-    </ul>
 </template>
 
 <script setup lang="ts">
-
-import { onMounted } from 'vue';
-import {useApartmentStore} from '../../stores/apartmentStore'
-
-const store = useApartmentStore()
-
-onMounted(async ()=>{
-    await store.getAll()
-    console.log(import.meta)
-})
 
 </script>
 
