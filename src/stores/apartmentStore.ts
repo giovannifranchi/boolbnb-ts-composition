ef7476340fd24bf05741594737798a68c624c080
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-07-18 23:49:50 +0200

    Add: integrate get services in apartment store

--- src/stores/apartmentStore.ts
+++ src/stores/apartmentStore.ts
@@ -1,7 +1,9 @@
 import { defineStore } from 'pinia'
 
 import Apartment from '@/api/Apartment'
+import Service from '@/api/Service'
 import type { ApartmentType } from '@/types/apartment-store/Apartment'
+import type { ServicesType } from '@/types/api-types/ApartmentQueryType'
 
 export const useApartmentStore = defineStore('apartmentStore', {
     
@@ -9,10 +11,12 @@ export const useApartmentStore = defineStore('apartmentStore', {
         return {
             allApartments: [] as ApartmentType[],
             highlighted: [] as ApartmentType[],
+            services: [] as ServicesType[]
         }
     },
 
     actions: {
+
         async getAll(){
             const result: ApartmentType[] = await Apartment.get();
             this.allApartments = result;
@@ -21,6 +25,11 @@ export const useApartmentStore = defineStore('apartmentStore', {
         async getHighlighted(){
             const result: ApartmentType[] = await Apartment.highlighted();
             this.highlighted = result
+        },
+
+        async getServices(){
+            const result: ServicesType[] = await Service.get();
+            this.services = result;
         }
     }
 
