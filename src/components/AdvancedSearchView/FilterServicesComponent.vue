a902ae959370844e37d31d1fc1036d383d331ad5
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-07-19 17:32:17 +0200

    Fix: service gap

--- src/components/AdvancedSearchView/FilterServicesComponent.vue
+++ src/components/AdvancedSearchView/FilterServicesComponent.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <h3>Services</h3>
-    <ul class="d-flex flex-wrap list-unstyled">
+    <ul class="d-flex flex-wrap list-unstyled gap-2">
       <li v-for="(service, index) in apartmentStore.services" :key="index">
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" @change="handleSelection(service.id)"/>
           <label class="form-check-label" for="flexCheckDefault">
             <FontAwesomeIcon :icon="service.icon_url"/>
+            {{ service.name }}
           </label>
         </div>
       </li>
